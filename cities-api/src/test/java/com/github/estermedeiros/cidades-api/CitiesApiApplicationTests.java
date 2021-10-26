411fe81e96fcfb3fad0fa97c6d76ab5cb4463c72
estermedeiros <93228763+estermedeiros@users.noreply.github.com>
2021-10-26 20:29:18 -0300

    Update CitiesApiApplicationTests.java

--- cities-api/src/test/java/com/github/estermedeiros/cidades-api/CitiesApiApplicationTests.java
+++ cities-api/src/test/java/com/github/estermedeiros/cidades-api/CitiesApiApplicationTests.java
@@ -1,4 +1,4 @@
-package com.github.lucasrmagalhaes.citiesapi;
+package com.github.estermedeiros.cidades-api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
