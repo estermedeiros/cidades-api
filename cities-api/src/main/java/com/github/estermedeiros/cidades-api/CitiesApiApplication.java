a8d30538690581bb486f3e25a179df1dd6cdc7c2
estermedeiros <93228763+estermedeiros@users.noreply.github.com>
2021-10-26 20:31:27 -0300

    Update CitiesApiApplication.java

--- cities-api/src/main/java/com/github/estermedeiros/cidades-api/CitiesApiApplication.java
+++ cities-api/src/main/java/com/github/estermedeiros/cidades-api/CitiesApiApplication.java
@@ -1,4 +1,4 @@
-package com.github.lucasrmagalhaes.citiesapi;
+package com.github.estermedeiros.cidades-api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
